76d4ad79f9fb4f3400c7f9f9111c1257ddc0ac89
Varun Parekh <123678616+chromaDesi@users.noreply.github.com>
2025-05-28 18:47:12 -0400

    Finalized info

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,14 +7,16 @@ const Projects = () => {
   const [activeTab, setActiveTab] = useState("projects");
   //add projects
   const work = [
-    { title: "Project 1", description: "This is project 1", skills: [], imgUrl: "/prop_model_r.jpg"},
-    { title: "Project 1", description: "This is project 1", skills: [], imgUrl: "/prop_model_r.jpg"},
-    { title: "Project 1", description: "This is project 1", skills: [], imgUrl: "/prop_model_r.jpg"},
+    { title: "FrequentFlier Rewards Android Application Tool ", description: "Created an Android app to allow easy access a custom database made in Oracle.", skills: ["Java", "OracleDB", "SQL", "Android", "JDBC API"], imgUrl: "/jdbc.jpg"},
+    { title: "Animated Digital Greeting Card ", description: "Hosted on AWS S3, a cute greeting card", skills: ["AWS S3", "JavaScript", "HTML5", "CSS"], imgUrl: "/vday.jpg", url: "https://surprisenia.s3.us-east-1.amazonaws.com/index.html"},
+    { title: "ZAKU Task Manager", description: "Implemented a custom task manager within Unix with chaining, dual processing, and file redirection", skills: ["C", "Unix"], imgUrl: "/zaku.jpg"},
+    { title: "Binary Sentiment Classification Model", description: "Evaluated product reviews to conduct sentiment analysis", skills: ["Python", "Scikit-Learn", "NLTK", "NumPy", "Pandas"], imgUrl: "/dm.png"},
+    { title: "Modified Tetris", description: "Recreated tetris with new mechanics", skills: ["Java", "OOP"], imgUrl: "/tetris.jpg"},
   ];
 
   const research = [
-    { title: "Cost-Sensitive Learning: Propensity Modeling Optimization", description: "#Python #Scikit-Learn #ML", skills: [], imgUrl: "/prop_model_r.jpg", url: "https://www.linkedin.com/in/vparekhinfo/details/projects/343267436/multiple-media-viewer/?profileId=ACoAADLKqvEBxeGRTptNKOI22SgKFr17oUha0Fc&treasuryMediaId=1735510055966" },
-    { title: "Marijuana and the Effects on the Young Adult", description: "#Healthcare", skills: [], imgUrl: "/weed.jpg", url: "https://www.linkedin.com/in/vparekhinfo/details/projects/351239392/multiple-media-viewer/?profileId=ACoAADLKqvEBxeGRTptNKOI22SgKFr17oUha0Fc&treasuryMediaId=1748402929164" },
+    { title: "Cost-Sensitive Learning: Propensity Modeling Optimization", description: "Evaluated and optimized various models to best model population propensity", skills: ["Python", "Scikit-learn", "NumPy", "Pandas"], imgUrl: "/prop_model_r.jpg", url: "https://www.linkedin.com/in/vparekhinfo/details/projects/343267436/multiple-media-viewer/?profileId=ACoAADLKqvEBxeGRTptNKOI22SgKFr17oUha0Fc&treasuryMediaId=1735510055966" },
+    { title: "Marijuana and the Effects on the Young Adult", description: "Research Proposal on evaluating the effects of Marijuana on Young Adults", skills: ["Healthcare"], imgUrl: "/weed.jpg", url: "https://www.linkedin.com/in/vparekhinfo/details/projects/351239392/multiple-media-viewer/?profileId=ACoAADLKqvEBxeGRTptNKOI22SgKFr17oUha0Fc&treasuryMediaId=1748402929164" },
   ];
 
   const data = activeTab === "projects" ? work : research;
