ec7faea3e2442d96111c0c20a751ad71efdd384a
Varun Parekh <123678616+chromaDesi@users.noreply.github.com>
2025-06-04 03:54:34 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { preloadAssets } from './libs/preload.js'
+import {SpeedInsights} from "@vercel/speed-insights/react"
 
 // Start preloading assets immediately
 preloadAssets();
